13d39a274f46d5206a4d96ce73f6aca62329e96e
Menma <mennaxmohiex@gmail.com>
2019-05-09 20:40:14 +0200

    Unfinished algorithm

--- AI-Package/MachineLearning.py
+++ AI-Package/MachineLearning.py
@@ -1,4 +1,5 @@
 import math
+import array
 
 
 class item:
@@ -44,198 +45,150 @@ class Feature:
 
 # incomplete
 class Node:
-    def __init__(self, name, value):
+    def __init__(self, name='', column_index=-1, pure=0, decision=-1):
         self.name = name
+        self.column_index = column_index
+        self.pure = pure
+        self.decision = decision
         self.right = None
         self.left = None
 
 class ID3:
     def __init__(self, features):
         self.features = features
-        self.data0 = dataset
-        self.data1 = []
         self.tree_root = None
+        self.data_2D = self.construct_2D_array(dataset)
+        self.build_decision_tree(self.data_2D, self.tree_root)
 
-    def info_gain(self, column_name, current_items):
+    def construct_2D_array(self, dataset):
+        data_2D = []
+        for i in range(0, len(dataset)):
+            lst = [dataset[i].age, dataset[i].prescription, dataset[i].astigmatic, dataset[i].tearRate, dataset[i].needLense]
+            data_2D.insert(i, lst)
+        return data_2D
+
+    def entropy(self, column_index, current_items):
         #  total entropy
         total = len(current_items)
         need = 0
-        noNeed = 0
-        for item in current_items:
-            if item.needLense == 0:
-                noNeed += 1
-            elif item.needLense == 1:
-                need += 1
-        total_entropy = ((-need / total) * math.log((need / total), 2)) + (
-                (-noNeed / total) * math.log((noNeed / total), 2))
-
-        if column_name == "age":
-            lense_young = 0
-            lense_old = 0
-            no_lense_young = 0
-            no_lense_old = 0
-            for item in current_items:
-                if item.needLense:
-                    if item.age == 0:
-                        lense_young += 1
-                    else:
-                        lense_old += 1
+        no_need = 0
+        zero_need = 0
+        one_need = 0
+        zero_no_need = 0
+        one_no_need = 0
+        decision_col = len(self.data_2D[0]) - 1
+        for i in range(0, len(current_items)):
+            if self.data_2D[i][decision_col] == 0:
+                no_need += 1
+                if self.data_2D[i][column_index]:
+                    one_no_need += 1
                 else:
-                    if item.age == 0:
-                        no_lense_young += 1
-                    else:
-                        no_lense_old += 1
-
-            total_young = lense_young + no_lense_young
-            p_young_lense = lense_young / total_young
-            p_young_no_lense = no_lense_young / total_young
-            young_entropy = -p_young_lense * math.log(p_young_lense, 2) - p_young_no_lense * math.log(p_young_no_lense, 2)
-
-            total_old = lense_old + no_lense_old
-            p_old_lense = lense_old / total_old
-            p_old_no_lense = no_lense_old / total_old
-            old_entropy = -p_old_lense * math.log(p_old_lense, 2) - p_old_no_lense * math.log(p_old_no_lense, 2)
-
-            return total_entropy - (total_young/total * young_entropy + total_old/total * old_entropy)
-
-        if column_name == "prescription":
-            lense_myope = 0
-            lense_hyper = 0
-            no_lense_myope = 0
-            no_lense_hyper = 0
-            for item in current_items:
-                if item.needLense:
-                    if item.prescription == 0:
-                        lense_myope += 1
-                    else:
-                        lense_hyper += 1
-                else:
-                    if item.prescription == 0:
-                        no_lense_myope += 1
-                    else:
-                        no_lense_hyper += 1
-
-            total_myope = lense_myope + no_lense_myope
-            p_myope_lense = lense_myope / total_myope
-            p_myope_no_lense = no_lense_myope / total_myope
-            myope_entropy = -p_myope_lense * math.log(p_myope_lense, 2) - p_myope_no_lense * math.log(p_myope_no_lense, 2)
-
-            total_hyper = lense_hyper + no_lense_hyper
-            p_hyper_lense = lense_hyper / total_hyper
-            p_hyper_no_lense = no_lense_hyper / total_hyper
-            hyper_entropy = -p_hyper_lense * math.log(p_hyper_lense, 2) - p_hyper_no_lense * math.log(p_myope_no_lense, 2)
-
-            return total_entropy - (total_myope / total * myope_entropy + total_hyper / total * hyper_entropy)
-
-        if column_name == "astigmatic":
-            lense_not_ast = 0
-            lense_ast = 0
-            no_lense_not_ast = 0
-            no_lense_ast = 0
-            for item in current_items:
-                if item.needLense:
-                    if item.astigmatic == 0:
-                        lense_not_ast += 1
-                    else:
-                        lense_ast += 1
-                else:
-                    if item.astigmatic == 0:
-                        no_lense_not_ast += 1
-                    else:
-                        no_lense_ast += 1
-
-            total_not_ast = lense_not_ast + no_lense_not_ast
-            p_not_ast_lense = lense_not_ast / total_not_ast
-            p_not_ast_no_lense = no_lense_not_ast / total_not_ast
-            not_ast_entropy = -p_not_ast_lense * math.log(p_not_ast_lense, 2) - p_not_ast_no_lense * math.log(p_not_ast_no_lense, 2)
-
-            total_ast = lense_ast + no_lense_ast
-            p_ast_lense = lense_ast / total_ast
-            p_ast_no_lense = no_lense_ast / total_ast
-            ast_entropy = -p_ast_lense * math.log(p_ast_lense, 2) - p_ast_no_lense * math.log(p_ast_no_lense, 2)
-
-            return total_entropy - (total_not_ast / total * not_ast_entropy + total_ast / total * ast_entropy)
-
-        if column_name == "tearRate":
-            lense_normal = 0
-            lense_reduced = 0
-            no_lense_normal = 0
-            no_lense_reduced = 0
-            for item in current_items:
-                if item.needLense:
-                    if item.tearRate == 0:
-                        lense_normal += 1
-                    else:
-                        lense_reduced += 1
-                else:
-                    if item.prescription == 0:
-                        no_lense_normal += 1
-                    else:
-                        no_lense_reduced += 1
-
-            total_normal = lense_normal + no_lense_normal
-            p_normal_lense = lense_normal / total_normal
-            p_normal_no_lense = no_lense_normal / total_normal
-            normal_entropy = -p_normal_lense * math.log(p_normal_lense, 2) - p_normal_no_lense * math.log(p_normal_no_lense, 2)
-
-            total_reduced = lense_reduced + no_lense_reduced
-            p_reduced_lense = lense_reduced / total_reduced
-            p_reduced_no_lense = no_lense_reduced / total_reduced
-            reduced_entropy = -p_reduced_lense * math.log(p_reduced_lense, 2) - p_reduced_no_lense * math.log(p_reduced_no_lense, 2)
-
-            return total_entropy - (total_normal / total * normal_entropy + total_reduced / total * reduced_entropy)
-
-    def split(self, column_name):
-        if column_name == 'age':
-            for item in self.data0:
-                if item.age:
-                    self.data0.remove(item)
-                    self.data1.append(item)
-            return
-
-        if column_name == 'prescription':
-            for item in self.data0:
-                if item.prescription:
-                    self.data0.remove(item)
-                    self.data1.append(item)
-            return
-
-        if column_name == 'astigmatic':
-            for item in self.data0:
-                if item.astigmatic:
-                    self.data0.remove(item)
-                    self.data1.append(item)
-            return
-
-        if column_name == 'tearRate':
-            for item in self.data0:
-                if item.tearRate:
-                    self.data0.remove(item)
-                    self.data1.append(item)
-            return
-
-    # incomplete
-    def build_decision_tree(self):
-        max_gain = 0
-        max_feature = Feature
-        for feature in self.features:
-            if feature.visited:
-                continue
-            if self.data0 is None:
-                feature.infoGain = self.info_gain(feature.name, dataset)
+                    zero_no_need += 1
             else:
-                feature.infoGain = self.info_gain(feature.name, self.data0)
-            if feature.infoGain > max_gain:
-                max_gain = feature.infoGain
-                max_feature = feature
+                need += 1
+                if self.data_2D[i][column_index]:
+                    one_need += 1
+                else:
+                    zero_need += 1
+        total_entropy = 0
+        if need != total and need != 0:
+            total_entropy += (-need / total) * math.log((need / total), 2)
+        if no_need != total and no_need != 0:
+            total_entropy += (-no_need / total) * math.log((no_need / total), 2)
 
-        self.split(max_feature.name)
-        max_feature.visited = 1;
+        zero_entropy = 0
+        total_zero = zero_need + zero_no_need
+        if total_zero > 0:
+            print(zero_need, zero_no_need)
+            p_zero_need = zero_need / total_zero
+            p_zero_no_need = zero_no_need / total_zero
+            if p_zero_need != 1 and p_zero_need != 0:
+                zero_entropy += -p_zero_need * math.log(p_zero_need, 2)
+            if p_zero_no_need != 1 and p_zero_no_need != 0:
+                zero_entropy += -p_zero_no_need * math.log(p_zero_no_need, 2)
+
+        one_entropy = 0
+        total_one = one_need + one_no_need
+        if total_one > 0:
+            print(one_need, one_no_need)
+            p_one_need = one_need / total_one
+            p_one_no_need = one_no_need / total_one
+            if p_one_need != 1 and p_one_need != 0:
+                one_entropy += -p_one_need * math.log(p_one_need, 2)
+            if p_one_no_need != 1 and p_one_no_need != 0:
+                one_entropy += -p_one_no_need * math.log(p_one_no_need, 2)
+
+        return total_entropy, total_zero, zero_entropy, total_one, one_entropy
+
+    def info_gain(self, column_index, current_items):
+        total_entropy, total_zero, zero_entropy, total_one, one_entropy = self.entropy(column_index, current_items)
+        print('ent:', total_entropy, total_zero, zero_entropy, total_one, one_entropy)
+        total = len(current_items)
+        gain = total_entropy - (total_zero / total * zero_entropy + total_one / total * one_entropy)
+        return gain, zero_entropy, one_entropy
+
+    def split(self, data, column_index):
+        data0 = []
+        data1 = []
+        for i in range(0, len(data)):
+            row = data[i]
+            if data[i][column_index] == 0:
+                data0.insert(i, row)
+            else:
+                data1.insert(i, row)
+        return data0, data1
+
+    def build_decision_tree(self, data, current_node):
+        max_gain = -1
+        max_feature = -1
+        max_feature_pure = 0
+        max_feature_decision = -1
+        for feature_index in range(0, len(self.features)):
+            if self.features[feature_index].visited == 1:
+                continue
+            gain, zero_entropy, one_entropy = self.info_gain(feature_index, data)
+
+            print(self.features[feature_index].name)
+            print('g:', gain)
+            self.features[feature_index].info_gain = gain
+            if gain > max_gain:
+                max_gain = gain
+                max_feature = feature_index
+                if zero_entropy == 0:
+                    max_feature_pure = 1
+                    max_feature_decision = 0
+                if one_entropy == 0:
+                    max_feature_pure = 1
+                    max_feature_decision = 1
+        if max_feature == -1:
+            return
+        print('tree: ', self.features[max_feature].name, max_feature)
+        current_node = Node(self.features[max_feature].name, max_feature, max_feature_pure, max_feature_decision)
+        print(current_node.name)
+        print(current_node.decision)
+        features[max_feature].visited = 1
+        if max_feature_pure:
+            return
+        data0, data1 = self.split(data, max_feature)
+        self.build_decision_tree(data0, current_node.left)
+        self.build_decision_tree(data1, current_node.right)
 
     def classify(self, input):
         # takes an array for the features ex. [0, 0, 1, 1]
         # should return 0 or 1 based on the classification
-        pass
+        current_node = self.tree_root
+        parent_node = current_node
+        while current_node is not None:
+            print("hi")
+            print(current_node.name)
+            print(current_node.decision)
+            print(current_node.column_index)
+            parent_node = current_node
+            if input[current_node.column_index]:
+                current_node = current_node.right
+            else:
+                current_node = current_node.left
+        return parent_node.decision
 
 
 dataset = getDataset()
